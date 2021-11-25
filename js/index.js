a1e7e9f073f4071e61205bb573803b31606a4ccb
unknown <ponomareve45@gmail.com>
2021-11-25 17:38:10 +0300

    Сделал модальное окно

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
+// //открытие и скрытие header
+// function toggletoggleCloseHeade() {
+//   document.getElementById("close").classList.toggle("close__header");
+// }
+// function toggleOpenHeader() {
+//   document.getElementById("close").classList.toggle("close__header");
+// }
 
-{//const bigBox = prompt('Привет друзья');
+const disabledScroll = () => {
+  //добавление отступа из-за отсутствия скрола
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+  document.body.scrollPosition = window.scrollY;
+  document.body.style.cssText = `
+  overflow: hidden;
+  
+  top: -${document.body.scrollPosition}px;
+  left: 0;
+  height: 100vh;
+  padding-right: ${widthScroll}px;
+  `;
+}
+const enabledScroll = () => {
+  document.body.style.cssText = '';
+  window.scroll({top: document.body.scrollPosition})
+}
 
+{
 const presentOrderBtn = document.querySelector('.present__order-btn');
 const pageOverlayModal = document.querySelector('.page__overlay_modal');
 const modalClose = document.querySelector('.modal__close');
@@ -24,37 +48,51 @@ const handlerModel = (openBtn, modal, openSelector, closeTrigger, sk) => {
     fast: 1,
     default: 3,
   };
-  //Вещаем на кнопку событие клика и сразу задаем событие открытие модалки
-  openBtn.addEventListener('click', () => {
-    //При открытие опасити равно 0
+const openModal = () => {
+  disabledScroll();
+  //При открытие опасити равно 0
+  modal.style.opacity = opacity
+  modal.classList.add(openSelector)
+
+  //Интервальная функция
+  const timer = setInterval(() => {
+    //Увкличивать опасити на 0.02
+    opacity += 0.02;
     modal.style.opacity = opacity
-    modal.classList.add(openSelector)
+    //Если опасити >=1 остановить timer
+    if (opacity >= 1) clearInterval(timer)
+    //Тернарный оператор. Если то что мы передали наййдется то вернется это число, если нет то андефайд(0-false/1 и далее true)
+  }, speed[sk] ? speed[sk] : speed.default)
+};
 
-    //Интервальная функция
-    const timer = setInterval(() => {
-      //Увкличивать опасити на 0.02
-      opacity += 0.02;
-      modal.style.opacity = opacity
-      //Если опасити >=1 остановить timer
-      if (opacity >= 1) clearInterval(timer)
-      //Тернарный оператор. Если то что мы передали наййдется то вернется это число, если нет то андефайд(0-false/1 и далее true)
-    }, speed[sk] ? speed[sk] : speed.default)
-  })
-  closeTrigger.addEventListener('click', () => {
+const closeModal =  () => {
+  enabledScroll();
+  const timer = setInterval(() => {
+    opacity -= 0.02;
+    modal.style.opacity = opacity;
+    if (opacity <=0) {
+    clearInterval(timer)
+    modal.classList.remove(openSelector)
+    }
+  }, speed[sk] ? speed[sk] : speed.default)
+};
 
-    const timer = setInterval(() => {
-      opacity -= 0.02;
-      modal.style.opacity = opacity;
-      if (opacity <=0) {
-      clearInterval(timer)
-      modal.classList.remove(openSelector)
-      }
-    }, speed[sk] ? speed[sk] : speed.default)
+  //Вещаем на кнопку событие клика и сразу задаем событие открытие модалки
+  openBtn.addEventListener('click', openModal);
+  closeTrigger.addEventListener('click', closeModal);
+
+  //event-объект события который создается в момент клика
+  modal.addEventListener('click', (event) =>{
+    if (event.target === modal) { 
+    closeModal()  
+    }
+
   })
 };
 handlerModel(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'slow');
 //Открыте и закрытие модального окна (все что выше!!!)
 }
+
 {
   const headerContactsBurger = document.querySelector('.header__contacts-burger');
   const headerContacts = document.querySelector('.header__contacts');
